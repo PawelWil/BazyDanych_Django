0ad9e087fa446a304e45611bef3ca1f63de067c6
PawelWil <119164049+PawelWil@users.noreply.github.com>
2023-05-16 16:17:55 +0200

    update

--- ZJAZD 6  (06-07.05.2023)/V. SQL !!/demo3.sql
+++ ZJAZD 6  (06-07.05.2023)/V. SQL !!/demo3.sql
@@ -1,33 +1,75 @@
 -- FUNKCJE SKALARNE
+---- Funkcje skalarne(przykłady):
+-- LEFT - pobiera znaki/ cyfry od lewej strony
+-- ISNULL - ktora przekształca dane które są nullem, na dane które dalej są nullem,
+  -- ale mają inną nazwę + dodatkowo można sobie we własny sposób nazwać kolumnę
+  -- SELECT Name, Color, ISNULL(Color, 'no color') AS SuperColor FROM Production.Product --tu mamy już aliasowanie,
+  -- czyli wyświetlamy kolumnę Name i Color, gdzie kolumna Color wszystkie wartości NULL wyświetla jako: no color +
+  -- wtedy w tej nowej kolumnie gdzie zamiast NULL jest 'no color' ta nowa dodatkowa kolumna zmienia nazwę na SuperColor
+  -- + oczywiście są wyświetlane wszystkie inne kolory
+-- SUBSTRING - wyciąga wycinek ciągu znaków, np. mamy ciąg ABCD, to za pomocą tej funkcji wyciągniemy np. wycinek BC
+-- YEAR - ona wyciąga tylko rok z kolumn, bez miesięcy/dni
 -- Transact-SQL !! Wprowadza dużą ilość różnych funkcji których nie damy rady omówić w całości -> i nikt by tego nie zapamiętał
--- Ważną informacją jest to że funkcję występujące w SQLServer nie muszą występować w innych RMDBS!!!!! -> T-SQL to rozwinięcie standardowego języka SQL
+-- Ważną informacją jest to że funkcję występujące w SQLServer nie muszą występować w innych RMDBS!!!!!
+   -- -> T-SQL to rozwinięcie standardowego języka SQL
+
+USE AdventureWorks2014
 
 -- wyciągnięcie roku z daty
-SELECT YEAR(SellStartDate) AS SellStartYear, ProductID, Name
+SELECT YEAR(SellStartDate) AS SellStart, SellStartDate, ProductID, Name
 FROM Production.Product
-ORDER BY SellStartYear;
+ORDER BY SellStart;
+--1. jak widać żeby tylko rok wyciągnąć, wpisujemy funkcję 'YEAR' + w nawiasie podajemy z jakiej kolumny
+-- ten rok ma być wyciągnięty + dalismy aliasowanie za pomocą 'AS', gdzie ta kolumna została nazwana jako
+-- 'SellStart'
+--2. Potem zrobiliśmy na samym koncu sortowanie 'Order by' po nowej kolumnie 'SellStart', która wyciąga tylko rok
+--3. Obok też podana ta kolumna 'SellStartDate', żeby było widać, że zawiera ona rok-miesiąc-dzien
+
 
 -- działania na dacie + GETDATE()
-SELECT YEAR(SellStartDate) AS SellStartYear,
-       DATENAME(mm,SellStartDate) AS SellStartMonth,
-       DAY(SellStartDate) AS SellStartDay,
-       DATENAME(dw, SellStartDate) AS SellStartWeekday,
-       DATEDIFF(yy,SellStartDate, GETDATE()) AS YearsSold,
+SELECT YEAR(SellStartDate) AS SellStartYear,  -- tu wyciągamy rok z kolumny SellStartDate
+       DATENAME(mm,SellStartDate) AS SellStartMonth, -- tu wyciągamy miesiąc, do kolumny SellStartMonth
+       DAY(SellStartDate) AS SellStartDay, -- tu wyciągamy numer dnia miesiąca, np.30
+       DATENAME(dw, SellStartDate) AS SellStartWeekday, -- tu wyciągamy nazwę dnia, np.  Wednesday
+       DATEDIFF(yy,SellStartDate, GETDATE()) AS YearsSold, -- tu mamy dwie funkcje 'DateDiff', która
+       -- wyciąga różnice lat pomiędzy obecną datą (obecną datę dostajemy za pomocą funkcji 'GetDate'), a datą
+       -- podaną w kolumnie SellStartDate, czyli sprzedaży. I dostajemy to w latach, np. 15.
+       -- i w tym przypadku używamy funkcję(GETDATE), w funkcji(DATEDIFF)
+       SellStartDate,
        ProductID,
        Name
 FROM Production.Product
 ORDER BY SellStartYear;
 
 -- Funkcje w funkcjach
-SELECT UPPER(Name) AS ProductName,
-       ProductNumber,
-       Weight,
-       ROUND(Weight, 0) AS ApproxWeight,
-       LEFT(ProductNumber, 2) AS ProductType,
-       SUBSTRING(ProductNumber,CHARINDEX('-', ProductNumber) + 1, 4) AS ModelCode
-FROM Production.Product;
+SELECT UPPER(Name) AS ProductName, -- Funkcja 'UPPER' przekształca wszystkie litery w kolumnie 'Name' na duże +
+                                   -- + zaliasowała ją na nową nazwę, czyli 'ProductName'
+       ProductNumber, -- tu niezależnie/normalnie wyświetlamy kolumnę 'ProductNumber'
+       Weight, -- tu niezależnie/normalnie wyświetlamy kolumnę 'Weight'
+       ROUND(Weight, 0) AS ApproxWeight, -- a tu wartości w kolumnie 'Weight' zaokrąglamy za pomocą funkcji 'ROUND',
+                                         -- do pełnej liczby, bo mamy '0', jakby było '1' byłoby zaokrąglenie do 1
+                                         -- liczby po przecinku. Czyli w naszym przypadku po zaokrągrleniu '0',
+                                         -- dostajemy liczbę bez przecinka, np.2.00 + aliasujemy ją jako
+                                         -- 'ApproxWeight'
+                                         -- ! WAŻNE: w SQL po liczbie zawsze dajemy '.'
+       LEFT(ProductNumber, 2) AS ProductType, -- tu stosując funkcję 'LEFT', pobieramy znaki z lewej strony,
+                                        -- w naszym przypadku pobieramy dwa pierwsze znaki + kolumnę aliasujemy
+                                        -- jako ProductType
+       SUBSTRING(ProductNumber,CHARINDEX('-', ProductNumber) + 1, 4) AS ModelCode -- tu robimy za pomocą funkcji
+       -- 'SUBSTRING' z kolumny 'ProductNumber' wyciągninęcie jakichś znaków i umieszczenie ich w innej kolumnie -
+       -- w kolumnie 'ModelCode'. ProductNumber wygląda następująco: FR-T98Y-54, czyli za pomocą funkji w funkcji -
+       -- u nas tą funkcją zagnieżdżoną jest funkcja 'CharIndex', która identyfikuje pierwszy dash= '-',
+       -- który jest znakiem 3, więc żeby zacząć wyciąganie od znaku 4, musimy dodać 1, i po przecinku podajemy
+       -- ile znaków wyciągamy, czyli podaliśmy 4, więc wyciągamy 4 znaki + wrzucamy te 4 znaki w nową zaaliaso-
+       -- waną kolumnę ''ModelCode'
+FROM Production.Product; -- dane bierzemy ze schematu Production i z tabeli Product
 
--- Funkcje agregujące sum, count, average, minimum, or maximum.
+---- Funkcje agregujące (przykłady):
+-- sum, - sumuje jakieś założone wartości - np. mamy kolumne z cenami, to zsumuje nam te ceny
+-- count, - sumuje nam np. wszystkie wiersze w tabeli
+-- average, - wyciąga średnią
+-- minimum, or maximum. - wyciąga minimalną lub maksymalną wartość
+
 
 SELECT COUNT(*) AS Products,
        COUNT(DISTINCT ProductSubcategoryID) AS SubCategories,
@@ -40,7 +82,13 @@ FROM Production.Product AS p
               ON p.ProductSubcategoryID = c.ProductSubcategoryID
 WHERE c.Name LIKE '%Bikes';
 
--- Grupowanie danych po wykonaniu funkcji agregujących
+
+
+------ Grupowanie danych po wykonaniu funkcji agregujących  -- 'GROUP BY' --
+-- pozwala zgrupować dane po kolumnach
+-- występuje wraz z funkcją agregującą, np. 'Count' lub 'Sum' lub 'Average' lub 'Min' lub 'Max'
+-- chcąc filtrować dane po zgrupowaniu, należy zamiast 'Where', używać 'Having'
+-- Kolumny, po których chcemy wykonać grupowanie muszą znajdować się po słowie 'Select'
 
 SELECT StoreID, COUNT(CustomerID) AS Customers
 FROM Sales.Customer
