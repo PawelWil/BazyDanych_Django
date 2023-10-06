2bea33bf8cb9099a5e1274c74761ea476b1c5796
PawelWil <119164049+PawelWil@users.noreply.github.com>
2023-10-06 16:13:19 +0200

    update

--- ZJAZD 6  (06-07.05.2023)/V. SQL !!/demo1.sql
+++ ZJAZD 6  (06-07.05.2023)/V. SQL !!/demo1.sql
@@ -32,8 +32,18 @@
 -- modyfikuje, albo zapisuje jako nowe, na danej bazie danych.
 
 -- ustawiamy bazę danych, na ktorej będziemy pracować - tu jest AdventureWorks jako bieżącą
--- żeby ją odpalić muszę tą linię 'USE AdventureWorks' uruchomić, poprzez jej zaznaczenie +
--- ctrl + ent
+-- ale zanim ją odpalimy musimy wybrać z tej bazy, jakie schematy nas interesują ->
+-- -> czyli w tą bazę wchodzę -> wciskam pole po prawej, gdzie jest zapisane: '0 of 18',
+-- czyli baza ma 18 schematów, ale zaczytanych jest 0 -> teraz jak już w nią wejdę zaznaczam
+-- te schematy, które mnie interesują i daje lewym myszki na bok, czyli np. zaczytma 6 schematów,
+-- to powinno sie pojawić '6 of 18' -> i dopiero teraz po zaczytaniu schematów z tej bazy danych
+-- mogę ją odpalić. Bez zaczytania schematów, odpalenie bazy danych nic by nie dało,
+-- bo przecież nic by nie zaczytało
+-- żeby ją odpalić muszę tą linię 'USE AdventureWorks' ZAZNACZYĆ i następnie za pomocą
+-- ctrl+ ent uruchomić.
+-- ! WAŻNE: Jeśli danej części query nie zaznaczę, to ta nie zaznaczona część
+-- się nie odpali!
+-- Również ctrl+ent używam do odpaleń queries
 USE AdventureWorks
 
 -- wyświetl wszystkie kolumny (*) z tabeli Product w schemacie Production
@@ -43,26 +53,31 @@ SELECT * FROM Production.Product
   -- tak jak poniżej --> '... FROM AdventureWorks.Production.Product', ale to się będzie tyczyło danych tylko
   -- dla tego jednego pobrania. Jak chcemy żeby cały czas była obsługiwana dana DB, to musimy poprzez
   -- polecenie 'USE' ją odpalić: '... USE AdventureWorks'
--- 'Production' to schemat (czyli takie nasze pudełko, w  którym są trzymane różne tabele)
--- 'Product' to tabela, w której są trzymane dane, które nas interesują
+-- 'PRODUCTION' = to schemat (czyli takie nasze pudełko, w  którym są trzymane różne tabele)
+-- 'PRODUCT' = to tabela, w której są trzymane dane, które nas interesują
 
 
 -- teraz przechodzimy do innej bazy danych
 USE master
 
--- 3-part table name (DB-SCHEMA-TABLE)
+-- 3-part table name (DB-SCHEMA-TABLE) --> tu podajemy 3-składniową ścieżkę, czyli:
+-- wybieram wszystkie kolumny = * z lokacji jak:
+-- Baza Danych = AdventureWorks + Schemat = Production + Tabele = Product
+-- a wiadomo, że nie wybieramy żadnych specjalnych kolumn  ztej tabeli 'Product',
+-- tylko wyciągamy wszystkie kolumny, bo daliśmy '*'
 SELECT * FROM AdventureWorks.Production.Product
 
 USE AdventureWorks
 
 -- teraz wyświetlamy 5 kolumn z tabeli Product (dajemy je po przecinku), schematu Production i wcześniej
 -- uruchomionej bazy danych 'AdventureWorks'
-SELECT ProductID, Name, ProductNumber, Color, StandardCost FROM Production.Product
+SELECT ProductID, Name, ProductNumber, Color, StandardCost
+FROM Production.Product
 
 
 -- Sortowanie wierszy za pomocą polecenia ORDER BY ASC | DESC
 SELECT ProductID, Name, Color FROM Production.Product
-ORDER BY Name
+ORDER BY Name --z defaultu to jest sortowanie alfabetyczne
 -- w zależności w jakiej kolejności sobie wpiszę kolumny, w takiej
                               -- kolejności one będą wyświetlane, czyli tu jako pierwsze ProductID, potem Name,
                               -- a na końcu Color
@@ -73,9 +88,9 @@ ORDER BY Name
     -- za pomocą instrukcji 'DESCEnD'
 
 SELECT Name, Color, ProductID FROM Production.Product
-ORDER BY Color, Name -- sortowanie najpierw po kolumnie Color, a potem po Name - ale to mówi nam, że wszystko
-                    -- się zaczyna od Color,a potem jak coś jest w tej samej kategprii Color do posortowania
-                    -- od strony Name jest sortowane
+ORDER BY Color, Name -- sortowanie najpierw po kolumnie Color, a potem po Name -
+-- ale to mówi nam, że wszystko się zaczyna od Color,a potem jak coś jest w tej samej
+-- kategprii Color do posortowania od strony Name jest sortowane
 
 -- the same result as above ze słowami ASC(ascending), które PyCharm zaznacza że są niepotrzebne, bo
 -- ta kolejność jest domyślna
@@ -91,12 +106,14 @@ ORDER BY Name DESC, Color -- jak widać możemy też sortować kolumny w rożny 
 -- Filtrowanie danych, robimy za pomocą słowa kluczowego 'WHERE'
 ----------------------------------------
 -- Odczytywanie produktów koloru czarnego
-SELECT * FROM Production.Product WHERE Color = 'Black' -- tu wyświetlamy wszystkie kolumny +
+SELECT * FROM Production.Product
+         WHERE Color = 'Black' -- tu wyświetlamy wszystkie kolumny +
 -- filtrujemy kolumne 'Color' po black, czyli wyświetli się tylko kolor 'black'
 
 
 SELECT ProductID, Name, ProductNumber, Color, ProductSubcategoryID
-FROM Production.Product WHERE Color = 'Black' -- tu robimy co powyższe, ale tylko dla 5 kolumn
+FROM Production.Product WHERE Color = 'Black'
+-- tu robimy co powyższe, ale tylko dla 5 kolumn, czyli zawęziliśmy ilość kolumn
 
 SELECT ProductSubcategoryID
 FROM Production.Product WHERE Color = 'Black'--! WAŻNE: my nawet możemy tej kolumny Color nie wyświetlać,
@@ -134,8 +151,8 @@ WHERE Name != 'Mountain Bikes' -- jest różna niż (not ISO standard)
 
 
 SELECT * FROM Production.ProductSubcategory
-WHERE ProductSubcategoryID <= 10 OR ProductSubcategoryID > 20 -- spodziewamy się po tej filtracji wyniku,
--- że ProdSubCatID będzie <=10 lub (or) >=20
+WHERE ProductSubcategoryID <= 10 OR ProductSubcategoryID > 20 -- spodziewamy się po tej
+-- filtracji wyniku, że ProdSubCatID będzie <=10 lub (or) >=20
 
 -- zakres wartosci
 SELECT * FROM Production.ProductSubcategory
@@ -153,11 +170,12 @@ WHERE Name = 'Mountain Bikes' -- tu filtrujemy po ciągu znaków, które w tym p
 
 -- % dowolna ilość (lub brak) dowolnego znaku za pomocą polecenia 'LIKE'
 SELECT * FROM Production.ProductSubcategory
-WHERE Name LIKE '%Bikes'  -- jeśli mamy '%Bikes' to weźmie nam wyfiltruje wszystko co ma Bikes na końcu, z różnymi
-                          -- przedrostkami przed nim, bo % jest przed nim -- czyli za Bikes już nic nie będzie,
-                          -- chyba że % będzie na koncu słowa, czyli 'Bikes%' - to wyfiltruje nam wszystkie slowa
-                          -- które się zaczną od Bikes, a za nim będą miały rożne ciągi znaków, którym również jest
-                          -- spacja
+WHERE Name LIKE '%Bikes'  -- jeśli mamy '%Bikes' to weźmie nam wyfiltruje wszystko co ma
+-- Bikes na końcu, z różnymi
+-- przedrostkami przed nim, bo % jest przed nim -- czyli za Bikes już nic nie będzie,
+-- chyba że % będzie na koncu słowa, czyli 'Bikes%' - to wyfiltruje nam wszystkie slowa
+-- które się zaczną od Bikes, a za nim będą miały rożne ciągi znaków, którym również jest
+-- spacja
 
 SELECT * FROM Production.ProductSubcategory
 WHERE Name LIKE 'C%'  -- tu filtrujemy kolumne Name, po słowach które się zaczynają od 'C', a za nim jest dowolny
@@ -170,33 +188,35 @@ WHERE Name LIKE '%es%' -- tu 'es' stałe, które może być na początku, końcu
 
 -- _ pojedynczy, dowolny znak
 SELECT * FROM Production.Product
-WHERE Name LIKE 'Flat Washer _' -- tu mamy: słowo 'Flat Washer' + 'spacja' + '_' to jest wyfiltrowanie po znaku
--- jeden dowolny OSOBNY znak (może być litera, cyfra),
--- np. Flat Washer 1
+WHERE Name LIKE 'Flat Washer _' -- tu mamy: słowo 'Flat Washer' + 'spacja' + '_'
+--, gdzie '_' to jest wyfiltrowanie po znaku, czyli jeden dowolny OSOBNY znak
+-- (może być litera, cyfra), np. Flat Washer 1
 
 
 -- [] PRZEKAZANIE listy znaków, które robimy za pomocą nawiasu kwadratowego
-SELECT * FROM Production.Product WHERE Name LIKE 'Classic Vest, [LM]' -- tu dostajemy słowo Classic Vest + spacja,
--- bo jak widać po przecinku jest spacja + pojedynczą literę 'L' albo 'M', czyli dostaniemy np. 'Classic Vest, L '
--- lub 'Classic Vest, M'
+SELECT * FROM Production.Product WHERE Name LIKE 'Classic Vest, [LM]' -- tu dostajemy słowo
+-- Classic Vest + spacja,
+-- bo jak widać po przecinku jest spacja + pojedynczą literę 'L' albo 'M', czyli dostaniemy
+-- np. 'Classic Vest, L ' lub 'Classic Vest, M'
 
-SELECT * FROM Production.Product WHERE Name LIKE 'Classic Vest, [K-Z]' -- zaś tu dostajemy słowo Classic Vest +
--- + spacja + pojedyncze litery z zakresu od K do Z
+SELECT * FROM Production.Product WHERE Name LIKE 'Classic Vest, [K-Z]' -- zaś tu dostajemy
+-- słowo Classic Vest + spacja + pojedyncze litery z zakresu od K do Z
 
 
 
 -- Operatory logiczne
 -- Red Subcategory: 2, 14, 31
 -- Kolejność operatorów
--- 1) AND - ma wyższe znaczenie niż OR, czyli jak będzie Or i And, to zawsze pierwszy będzie rozpatrywany argument
-    -- który jest po And, a dopiero potem ten po Or.
+-- 1) AND - ma wyższe znaczenie niż OR, czyli jak będzie Or i And, to zawsze pierwszy będzie
+-- rozpatrywany argument, który jest po And, a dopiero potem ten po Or.
 -- 2) OR
 
 
--- W poniższym przypadku jak widzimy Color 'Black' jest połączony z 'ProductSubCatID' za pomocą AND,więc dostaniemy
+-- W poniższym przypadku jak widzimy Color 'Black' jest połączony z 'ProductSubCatID'
+-- za pomocą AND,więc dostaniemy
 -- wszystkie kolory 'black' z ProductSubcategoryID = 1,
--- zaś Color 'Red' nie jest połączony z ProdSubCatID=1, bo po sobie ma OR, więc dostaniemy kolor 'red'
--- ale dla wszystkich ProdSubCatID = 1,12,3,4,5,6 itd.
+-- zaś Color 'Red' nie jest połączony z ProdSubCatID=1, bo po sobie ma OR, więc dostaniemy
+-- kolor 'red', ale dla wszystkich ProdSubCatID = 1,12,3,4,5,6 itd.
 -- ALe można to też załatwić za pomocą nawiasu, który damy tam gdzie poszczególne argumenty chcemy ze sobą połączyć.
 SELECT Color, ProductSubcategoryID, * FROM Production.Product
 WHERE Color = 'Red' OR Color = 'Black' AND ProductSubcategoryID = 1
@@ -208,7 +228,8 @@ WHERE Color = 'Black' OR Color = 'Red' -- tu prosto - dostajemy wszystkie warian
 
 -- !!! wykluczający się warunek- kolumna może mieć tylko jedną wartość
 SELECT Name, Color FROM Production.Product
-WHERE Color = 'Black' AND Color = 'Red' -- tu jest wykluczenie polegające na tym, że chcemy kolumnę Name i Color,
+WHERE Color = 'Black' AND Color = 'Red' -- tu jest wykluczenie polegające na tym,
+-- że chcemy kolumnę Name i Color,
 -- ale nie możemy dać do tej samej kolumny 'And' dla dwóch argumentów, bo przecież kolor nie może być czerwony
 -- i czarny jednocześnie.. ;-).
 
@@ -220,8 +241,8 @@ WHERE ProductSubcategoryID = 2 AND Color = 'Red' -- tu już jest ok, bo są dwie
 
 
 SELECT * FROM Production.Product
-WHERE ProductSubcategoryID = 2 AND NOT Color = 'Red' -- tu mamy wyniki, gdzie mamy PrudSubCatID=2 i NIE kolory
--- czerwone
+WHERE ProductSubcategoryID = 2 AND NOT Color = 'Red' -- tu mamy wyniki, gdzie mamy
+-- ProdSubCatID=2 i NIE kolory czerwone
 -- taki sam wynik co powyżej, w troszkę inny sposób, poprzez dodanie znaku różny '<>':
 SELECT * FROM Production.Product
 WHERE ProductSubcategoryID = 2 AND Color <> 'Red'
@@ -229,7 +250,8 @@ WHERE ProductSubcategoryID = 2 AND Color <> 'Red'
 
 SELECT * FROM Production.Product
 WHERE Color = 'Black' OR Color= 'Silver' OR Color  = 'Red' -- tu mamy dłuższą wersję różnych kolorów
--- taki sam wynik, ale poprzez krótszą wersję, za pomocą IN i w nawiasie okrągłym podajemy argumenty do filtrowania
+-- taki sam wynik, ale poprzez krótszą wersję, za pomocą IN i w nawiasie okrągłym
+-- podajemy argumenty do filtrowania
 SELECT * FROM Production.Product
 WHERE Color IN ('Black', 'Silver', 'Red')
 
@@ -242,8 +264,9 @@ SELECT * FROM Production.Product
 WHERE Color <> ('Black', 'Silver', 'Red') -- tak nie zadziała - operator różnicy nie może być użyty, musi byc NOT IN
 
 
--- nawiasy - a teraz dzięki nawiasom można zdecydować kolejnośc wykonywania operacji. Wiadomo, że w nawiasie się
--- rzeczy wykonają razem. Czyli poniżej dwa podwójne warunki się wykonają
+-- nawiasy - a teraz dzięki nawiasom można zdecydować kolejnośc wykonywania operacji.
+-- Wiadomo, że w nawiasie się rzeczy wykonają razem.
+-- Czyli poniżej dwa podwójne warunki się wykonają
 SELECT ProductSubcategoryID, Color FROM Production.Product
 WHERE (ProductSubcategoryID = 2 AND Color <> 'Red') -- ProdSubCATID=2 wszystko różne od red +
 -- ProdSubCATID=1 wszystko rożne od black
@@ -251,7 +274,8 @@ WHERE (ProductSubcategoryID = 2 AND Color <> 'Red') -- ProdSubCATID=2 wszystko r
 
 -- można tez taki warunek zastosować:
 SELECT Color, ProductSubcategoryID, * FROM Production.Product
-WHERE (Color IN ('Red', 'Black')) AND ProductSubcategoryID = 1 -- kolor red i black dla ProdSubCat=1
+WHERE (Color IN ('Red', 'Black')) AND ProductSubcategoryID = 1 -- kolor red i black dla
+-- ProdSubCat=1
 
 -- Lub to powyższe w ten sposób:
 SELECT Color, ProductSubcategoryID, * FROM Production.Product
@@ -261,9 +285,9 @@ WHERE (Color = 'Red' OR Color = 'Black') AND ProductSubcategoryID = 1
 SELECT ProductSubcategoryID, Color FROM Production.Product
 WHERE (ProductSubcategoryID = 2 AND Color <> 'Red')
  OR (ProductSubcategoryID = 1 AND Color <> 'Black')
-ORDER BY ProductSubcategoryID, Color -- i jeszcze sortowanie po ProdSubCatID rosnąco, a następnie
--- te powtarzalne po kolorze też rosnąco (bo wiadomo, że jak nie podajemy Desc, to z defaultu jest brane ASC,
--- czyli rosnące
+ORDER BY ProductSubcategoryID, Color -- i jeszcze sortowanie po ProdSubCatID rosnąco,
+-- a następnie te powtarzalne po kolorze też rosnąco (bo wiadomo, że jak nie podajemy Desc,
+-- to z defaultu jest brane ASC, czyli rosnące
 
 
 
@@ -280,8 +304,9 @@ WHERE ProductSubcategoryID BETWEEN 10 AND 19
 SELECT * FROM Sales.SalesOrderHeader
 
 SELECT * FROM Sales.SalesOrderHeader
-WHERE OrderDate BETWEEN '20040501' AND '20040507' -- tu filtrujemy po zakresie daty BETWEEN x AND y
--- taki sam wynik jak powyżej, dzięki użyciu porównań, ale powyższa składnia lepsza/preferowana
+WHERE OrderDate BETWEEN '20040501' AND '20040507' -- tu filtrujemy po zakresie daty BETWEEN
+-- x AND y -- taki sam wynik jak powyżej, dzięki użyciu porównań, ale powyższa składnia
+-- lepsza/preferowana
 SELECT * FROM Sales.SalesOrderHeader
 WHERE OrderDate >= '20040501' AND OrderDate <= '20040507'
 
@@ -289,14 +314,18 @@ WHERE OrderDate >= '20040501' AND OrderDate <= '20040507'
 SELECT * FROM Sales.SalesOrderHeader
 WHERE OrderDate >= '2004-05-01' AND OrderDate <= '2004-05-07'
 
--- * Generalnie SQL sobie poradził zarówno z datą napisaną jako: '20040501' oraz jako '2004-05-01'.
+-- * Generalnie SQL sobie poradził zarówno z datą napisaną jako: '20040501'
+-- oraz jako '2004-05-01'.
 -- Zapisał je w taki sam sposób.
 
 
--- jeszcze inaczej dla T-SQL (to jest płatny silnik, zrobiony przez Microsoft) - to jest rozwinięcie dla SQL przez
--- Microsofta - i np. na PostgreSQL nie zadziała, bo Microsoft chciał mieć bardziej rozbudowany silnik,
--- gdzie PostgreSQL tego nie ma. Ale jest jedna duża różnica, T-SQL jest płatny i to nie mało, a wcale nie tak dużo
--- lepszy od PostgreSQL, który jest darmowy, dlatego nawe duże korporacje migrują swoje bazy danych do PostgreSQL,
+-- jeszcze inaczej dla T-SQL (to jest płatny silnik, zrobiony przez Microsoft) - to jest
+-- rozwinięcie dla SQL przez
+-- Microsofta - i np. na PostgreSQL nie zadziała, bo Microsoft chciał mieć bardziej
+-- rozbudowany silnik,
+-- gdzie PostgreSQL tego nie ma. Ale jest jedna duża różnica, T-SQL jest płatny i to nie mało,
+-- a wcale nie tak dużo nie jest lepszy od PostgreSQL, który jest darmowy, dlatego nawe
+-- duże korporacje migrują swoje bazy danych do PostgreSQL,
 -- który je obsługuje, wiadomo jako silnik.
 SELECT * FROM Sales.SalesOrderHeader
 WHERE OrderDate BETWEEN CAST('2004-05-01' as date) AND CAST('2004-05-07' as date) -- raczej nie zadziała na PostgreSQL
@@ -340,9 +369,10 @@ SELECT CustomerID, OrderDate, ShipDate
 FROM Sales.SalesOrderHeader
 
 SELECT CustomerID AS cliente, OrderDate AS [fecha de pedido], ShipDate AS [fecha de entrega]
-FROM Sales.SalesOrderHeader -- zmieniamy nazwy kolumny na nowe, np. CustomerID na cliente itd.
--- Ważna rzecz, że jak zmieniamy nazwy kolumn (aliasujemy je) te nowe nazwy muszą być albo w nawiasach kwadratowych
--- albo w apostrofach albo bez niczego (bez apostrofów i bez nawiasów kwadratowych]
+FROM Sales.SalesOrderHeader -- zmieniamy nazwy kolumny na nowe, np. CustomerID na cliente i
+-- td.
+-- Ważna rzecz, że jak zmieniamy nazwy kolumn (aliasujemy je) te nowe nazwy muszą być albo
+-- w nawiasach kwadratowych albo w apostrofach albo bez niczego (bez apostrofów i bez nawiasów kwadratowych]
 
 -- tabele również możemy aliasować (później..), na razie korzystamy max z 1 tabeli na zapytanie :)
 
@@ -353,7 +383,8 @@ FROM Sales.SalesOrderHeader -- zmieniamy nazwy kolumny na nowe, np. CustomerID n
 -- które jeszcze są posortowane(ORDER BY), po malejącej dacie
 SELECT TOP 5 * FROM HumanResources.Employee ORDER BY ModifiedDate DESC
 
--- FUNKCJA COUNT zlicza nam ilość wierszy w tabeli Employee i zapisuję tą daną w kolumnie o nazwie 'employeeCnt'
+-- FUNKCJA COUNT zlicza nam ilość wierszy w tabeli Employee i zapisuję tą daną
+-- w kolumnie o nazwie 'employeeCnt'
 SELECT COUNT(*) AS employeesCnt FROM HumanResources.Employee
 
 -- Użycie FUNKCJI TOP w postaci procentowej (np. daj mi 10% TOP=górnych=pierwszych wierszy,
@@ -367,9 +398,10 @@ SELECT TOP 10 PERCENT * FROM HumanResources.Employee ORDER BY ModifiedDate DESC
 SELECT * FROM Person.Contact -- tu sobie wyświetlamy wszystkie kolumny ze schematu Person i tabeli Contact
 
 SELECT FirstName, LastName, FirstName + ' ' + LastName AS FullName
-FROM Person.Contact -- tu wyświetlamy sobie kolumnę FirstName, LastName + teraz używamy konkatenacji żeby połączyć
--- te 2 kolumny w jedną, która będzie od razu wyświetlać imię i nazwisko + zmieniamy jej nazwę za pomocą Aliasu (AS)
--- na FullName
+FROM Person.Contact -- tu wyświetlamy sobie kolumnę FirstName, LastName
+-- + teraz używamy konkatenacji żeby połączyć
+-- te 2 kolumny w jedną, która będzie od razu wyświetlać imię i nazwisko
+-- + zmieniamy jej nazwę za pomocą Aliasu (AS) na FullName
 
 
 -- Funkcja LEFT, która pobiera znaki od lewej strony
